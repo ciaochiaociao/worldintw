eacfb005df5c3c648891586a38f382245ab3a0da
Chiao <ton790927@gmail.com>
2018-06-22 13:18:06 +0800

    Make WordPress i18n and add contact form post type

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 require get_template_directory() . '/inc/function-admin.php';
 require get_template_directory() . '/inc/function-theme-support.php';
+require get_template_directory() . '/inc/function-contact-form.php';
 require get_template_directory() . '/inc/enqueue.php';
+load_theme_textdomain( 'worldintw', get_template_directory() . '/languages' );
 ?>
--- inc/function-contact-form.php
+++ inc/function-contact-form.php
@@ -8,33 +8,65 @@ THEME SUPPORT PAGE
 =============
 
 */
-
-function worldintw_add_theme_support(){
-	// Add Post Formats Theme Support
-	$postFormats = get_option( 'post_formats' );
-	$output = array();
-	if( !empty($postFormats)){
-		foreach ($postFormats as $format => $value) {
-			if ($value) {
-				$output[] = $format; //array push/append
-			}
-		}
-		add_theme_support( 'post-formats', $output );
-	}
-	
-	// Add Custom Header Theme Support
-	$customHeader = esc_attr(get_option( 'custom_header' ));
-	if ($customHeader == 1){
-		add_theme_support( 'custom-header' );
-	}
-
-	// Add Custom Background Theme Support
-	$customBackground = esc_attr(get_option( 'custom_background' ));
-	if ($customBackground == 1){
-		add_theme_support( 'custom-background' );
-	}
-
+$activate = esc_attr(get_option( 'activate_contact_form' ));
+if ($activate == 1){
+	add_action( 'init', 'worldintw_custom_post_type');
 }
-add_action( 'after_setup_theme', 'worldintw_add_theme_support');
+
+function worldintw_custom_post_type(){
+	/**
+	 * Registers a new post type
+	 * @uses $wp_post_types Inserts new post type object into the list
+	 */
+	
+	$labels = array(
+		'name'               => __( 'New Messages', 'worldintw' ),
+		'singular_name'      => __( 'New Message', 'worldintw' ),
+		'add_new'            => _x( 'Add New Message', 'Add some new messages', 'worldintw' ),
+		'add_new_item'       => __( 'Add New Message item', 'worldintw' ),
+		'edit_item'          => __( 'Edit Message', 'worldintw' ),
+		'new_item'           => __( 'New Message', 'worldintw' ),
+		'view_item'          => __( 'View Message', 'worldintw' ),
+		'search_items'       => __( 'Search Messages', 'worldintw' ),
+		'not_found'          => __( 'No Messages found', 'worldintw' ),
+		'not_found_in_trash' => __( 'No Messages found in Trash', 'worldintw' ),
+		'menu_name'          => __( 'Messages', 'worldintw' ),
+	);
+
+	$args = array(
+		'labels'              => $labels,
+		'hierarchical'        => false,
+		'description'         => 'description',
+		'taxonomies'          => array(),
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 26,
+		'menu_icon'           => true,
+		'show_in_nav_menus'   => true,
+		'publicly_queryable'  => true,
+		'exclude_from_search' => false,
+		'has_archive'         => true,
+		'query_var'           => true,
+		'can_export'          => true,
+		'rewrite'             => true,
+		'capability_type'     => 'post',
+		'supports'            => array(
+			'title',
+			'editor',
+			'author',
+			'thumbnail',
+			'excerpt',
+			'custom-fields',
+			'trackbacks',
+			'comments',
+			'revisions',
+			'page-attributes',
+			'post-formats',
+		)
+	);
+	register_post_type( 'contact_post_type', $args );
+}
 
 ?>
