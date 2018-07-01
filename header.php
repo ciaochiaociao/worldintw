1c33247993907ac6adf393409dd476bc866fee8c
Chiao <ton790927@gmail.com>
2018-07-02 02:07:27 +0800

    Fix bugs: show pingback link tag only when post is singular and pings are open

--- header.php
+++ header.php
@@ -16,7 +16,9 @@ This is the template for the header
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width; initial-scale=1.0">
 		<link rel="profile" href="https://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php if(is_singular() && pings_open( get_queried_object() ) ): ?>
+			<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<?php endif; ?>
 		<?php wp_head(); ?>
 	</head>
 <body <?php body_class(); ?>>
