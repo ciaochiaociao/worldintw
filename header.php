18994172df08838edf9d85d90783e9d3d3afd417
Chiao <ton790927@gmail.com>
2018-07-07 19:14:31 +0800

    Design header and add custom worldintw icon font with icomoon

--- header.php
+++ header.php
@@ -23,4 +23,29 @@ This is the template for the header
 		<?php endif; ?>
 		<?php wp_head(); ?>
 	</head>
-<body <?php body_class(); ?>>
+	<body <?php body_class(); ?>>
+		<div class="container">
+			
+			<div class="row">
+				<div class="col-xs-12">
+					
+					<div class="header-container background-image text-center" style="background-image: url(<?php header_image(); ?>);">
+						
+						<div class="header-content table">
+							<div class="table-cell">
+								<h1 class="site-title worldintw">
+									<span class="worldintw-logo"></span>
+									<span class="hide"><?php bloginfo( 'name' ); ?></span>
+								</h1>
+								<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+							</div><!-- .table-cell -->
+						</div><!-- .header-content -->
+						
+						<div class="nav-container"></div><!-- .nav-container -->
+						
+					</div><!-- .header-container -->
+					
+				</div><!-- .col-xs-12 -->
+			</div><!-- .row -->
+			
+		</div><!-- .container-fluid -->
